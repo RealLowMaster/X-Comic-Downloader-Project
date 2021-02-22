21a67908f84d5aa3e93aacdbc3748a07f25c48c3
Mehdi Enayatzadeh <wamofmaster@gmail.com>
2021-02-22 12:19:12 +0330

    Create Main Body Part 1

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+const xlecx = new XlecxAPI();
+
+
+
+$(document).ready(() => {
+	// var h = xlecx.getPage(692, true);
+	// var h = xlecx.getComic('9666-uchishikiri.html');
+	// var h = xlecx.getAllTags();
+	// var h = xlecx.getGroup('pucchu', 1);
+	// var h = xlecx.getArtist('felsala', 1);
+	// var h = xlecx.getParody('boruto', 1);
+	// var h = xlecx.getTag('absorption', 2);
+	// var h = xlecx.search('hello');
+});
