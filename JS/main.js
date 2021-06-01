5b7feac376b0a21220b2ed0163f6b08c14d17f95
Mehdi Enayatzadeh <wamofmaster@gmail.com>
2021-06-01 19:34:44 +0430

    make basic tab resize

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,35 @@
 const xlecx = new XlecxAPI();
 
+function openBrowser() {
+	console.log('Comming Sood!')
+}
 
+function closeBrowser() {
+	console.log('Comming Sood!')
+}
+
+function openXlecxBrowser() {
+	$('.browser').attr('style', 'display:grid');
+}
+
+function updateTabSize() {
+	var windowWidth = window.innerWidth
+	var tabs = document.getElementById('browser-tabs').getElementsByTagName('div')
+	if ((windowWidth / 200) <= tabs.length) {
+		var tabWidth = (windowWidth - 60) / tabs.length
+		for (var i = 0; i < tabs.length; i++) {
+			tabs[i].style.width = tabWidth+'px'
+		}
+	} else {
+		for (var i = 0; i < tabs.length; i++) {
+			tabs[i].style.width = '200px'
+		}
+	}
+}
+
+setInterval(() => {
+	updateTabSize()
+}, 2000)
 
 $(document).ready(() => {
 	// var h = xlecx.getPage(692, true);
@@ -12,7 +41,3 @@ $(document).ready(() => {
 	// var h = xlecx.getTag('absorption', 2);
 	// var h = xlecx.search('hello');
 });
-
-function openXlecxBrowser() {
-	$('.browser').attr('style', 'display:grid');
-}
