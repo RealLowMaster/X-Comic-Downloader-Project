afba2d677d1bcb8eaf453519d4f7a924325dca29
Mehdi Enayatzadeh <wamofmaster@gmail.com>
2021-06-01 20:41:43 +0430

    make removeTab() and make updateTabSize() work better

--- JS/main.js
+++ JS/main.js
@@ -27,6 +27,10 @@ function updateTabSize() {
 	}
 }
 
+window.onresize = () => {
+	updateTabSize()
+}
+
 function activeTab(who, id) {
 	var pageId = who.getAttribute('pi')
 	page = document.getElementById(pageId) || null
@@ -41,6 +45,37 @@ function activeTab(who, id) {
 	tabsContainer.setAttribute('pid', pageId)
 	who.setAttribute('active', true)
 	document.getElementById(pageId).setAttribute('style', 'display:block')
+}
+
+function checkTabLimit() {
+	var limit = 35
+	var tabs = document.getElementById('browser-tabs').getElementsByTagName('div').length
+
+	if (tabs >= limit)
+		return true
+	else
+		return false
+}
+
+function createNewTab() {
+	if (checkTabLimit()) return null
+
+	var date = new Date()
+	var randomNumber = Math.floor(Math.random() * 500)
+	var newTabId = `${date.getTime()}-${randomNumber}`
+	var page = document.createElement('div')
+	page.setAttribute('class', 'browser-page')
+	page.setAttribute('id', newTabId)
+	document.getElementById('browser-tabs').innerHTML += `<div class="browser-tab" onclick="activeTab(this)" pi="${newTabId}"><span>${newTabId}</span> <button onclick="removeTab('${newTabId}')">X</button></div>`
+	document.getElementById('browser-pages').appendChild(page)
+
+	updateTabSize()
+	return newTabId
+}
+
+function removeTab(id) {
+	document.getElementById(id).remove()
+	document.getElementById('browser-tabs').querySelector(`[pi="${id}"]`).remove()
 }
 
 $(document).ready(() => {
