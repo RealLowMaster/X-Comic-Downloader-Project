e7e8e10ceb257b7d5867839dc43ba6e6b31e5935
Mehdi Enayatzadeh <wamofmaster@gmail.com>
2021-06-03 21:28:14 +0430

    Remove Deleting tmp Folder With index.js and convert space To Tab

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 const { app, BrowserWindow } = require('electron')
-const fs = require('fs')
 require('v8-compile-cache');
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    minWidth: 800,
-    minHeight: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  })
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		minWidth: 800,
+		minHeight: 600,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false
+		}
+	})
 
-  win.maximize(true)
+	win.maximize(true)
 
-  win.loadFile('index.html')
+	win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  createWindow()
+	createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
 })
 
 app.on('window-all-closed', () => {
-	fs.rmdir('./tmp', (err) => {
-		console.log(err)
-	})
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
